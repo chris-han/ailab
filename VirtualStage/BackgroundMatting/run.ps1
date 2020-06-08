e36eabe3f8ab50f6aeb63092f6ba9f35fc6d6d8f
Chris Han <chha@microsoft.com>
2020-06-08 18:11:04 +0800

    update local folder

--- VirtualStage/BackgroundMatting/run.ps1
+++ VirtualStage/BackgroundMatting/run.ps1
@@ -2,8 +2,8 @@ conda activate bgmatting
 $Env:CUDA_VISIBLE_DEVICES=0
 python .\bg_matting.py `
   --name "Speaker" `
-  --input D:\Speaker\ `
-  --output_dir D:\Speaker\output\ `
+  --input C:\Users\chha\Videos\Captures\My Recorded Sessions\2020-08-6-17-29-19\ `
+  --output_dir C:\Users\chha\Videos\Captures\My Recorded Sessions\2020-08-6-17-29-19\output\ `
   --fixed_threshold "640," `
   --start 34 `
   --duration 234

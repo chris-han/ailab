9187e4b6c4af0e6a1532a8388590ac0baff63442
Chris Han <chha@microsoft.com>
2020-06-14 23:35:16 +0800

    split and merge target background video

--- VirtualStage/BackgroundMatting/run.ps1
+++ VirtualStage/BackgroundMatting/run.ps1
@@ -2,10 +2,10 @@ conda activate bgmatting
 $Env:CUDA_VISIBLE_DEVICES=0
 python .\bg_matting.py `
   --name "Speaker" `
-  --input "C:\\Users\\chha\\Videos\\Captures\\My Recorded Sessions\\2020-08-6-17-29-19\\" `
-  --output_dir "C:\\Users\\chha\\Videos\\Captures\\My Recorded Sessions\\2020-08-6-17-29-19\\output\\" `
+  --input "C:\\Users\\chha\\Videos\\Captures\\My Recorded Sessions\\2020-12-6-12-26-15\\" `
+  --output_dir "C:\\Users\\chha\\Videos\\Captures\\My Recorded Sessions\\2020-12-6-12-26-15\\output\\" `
   --fixed_threshold "640," `
-  --output_types "out,matte,compose,fg" `
+  --output_types "out,matte,compose" `
   --start 0 `
   --duration -1 `
   --background "C:\\Users\\chha\\Videos\\Microsoft.ZuneVideo_8wekyb3d8bbwe!Microsoft.ZuneVideo"
